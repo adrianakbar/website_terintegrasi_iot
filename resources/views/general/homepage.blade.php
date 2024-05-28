dcaf422759e3d468ffec371261514ecf65f6d0e1
adrianakbar <armojkece123@gmail.com>
2024-05-28 10:20:04 +0700

    28mei

--- resources/views/general/homepage.blade.php
+++ resources/views/general/homepage.blade.php
@@ -29,19 +29,6 @@
           </h5>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-center flex-grow-1 pe-5">
-            <li class="nav-item">
-              <a class="nav-link mx-lg-3" aria-current="page" href="#">Data Kelembaban</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-lg-3" href="#">List Tugas</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-lg-3" href="#">Data Karyawan</a>
-            </li>
-          </ul>
-        </div>
       </div>
       <a href="/login" class="login-button">Login</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
